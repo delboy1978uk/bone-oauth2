43f9187d7e3309cd841c4f07cf39e46f852a7cef
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-05-27 16:28:26 +0200

    redirect uris in form

--- src/Form/RegisterClientForm.php
+++ src/Form/RegisterClientForm.php
@@ -15,7 +15,7 @@ class RegisterClientForm extends Form
 {
     public function init()
     {
-        $redirect = new Text('redirect_uri');
+        $redirect = new Text('redirect_uris');
         $redirect->setRequired(true);
         $validator = new ValidatorAdapterZf(new Uri());
         $redirect->addValidator($validator);
