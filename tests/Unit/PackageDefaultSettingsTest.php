94f9d26829141ff78b42247ef3906a7323275cd2
Derek Stephen McLean <delboy1978uk@gmail.com>
2026-02-21 15:13:06 +0100

    add github actions

--- tests/Unit/PackageDefaultSettingsTest.php
+++ tests/Unit/PackageDefaultSettingsTest.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+function fileperms($filename)
+{
+    return 0600;
+}
+
 namespace Tests\Unit;
 
 use Barnacle\Container;
@@ -26,7 +31,6 @@ use Del\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use League\OAuth2\Server\AuthorizationServer;
 use League\OAuth2\Server\ResourceServer;
-use League\OAuth2\Server\CryptKey;
 
 class PackageDefaultSettingsTest extends Unit
 {
@@ -94,8 +98,8 @@ class PackageDefaultSettingsTest extends Unit
             'authCodeTTL' => null,
             'accessTokenTTL' => null,
             'refreshTokenTTL' => null,
-            'privateKeyPath' => new CryptKey($keyPath, null, false),
-            'publicKeyPath' => new CryptKey($pubKeyPath, null, false),
+            'privateKeyPath' => getcwd() . '/tests/Support/Data/private.key',
+            'publicKeyPath' => getcwd() . '/tests/Support/Data/public.key',
             'encryptionKey' => 'def000002e113a725ebc60dc305541e09588776f65a17cf3258d8f7194bc3c38f62b0fe818cc026833bd1226b52e721534dee4e9db832977e1bc9ce764b848ad9fb3581f',
         ];
         $this->container['oauth2'] = $settings;
@@ -115,12 +119,4 @@ class PackageDefaultSettingsTest extends Unit
         $resourceServer = $this->container->get(ResourceServer::class);
         $this->assertInstanceOf(ResourceServer::class, $resourceServer);
     }
-}
-
-namespace League\OAuth2\Server;
-
-// Mock fileperms to bypass permission check in CryptKey
-function fileperms($filename)
-{
-    return 0600;
 }
